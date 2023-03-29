683c188cfdd0ff541a8bea329eec8231aa6d9d69
Sayed Mahmud <armanemon5801@gmail.com>
2023-03-29 23:19:13 +0600

    first commit

---
+++ lama-admin-jun/src/App.js
@@ -0,0 +1,41 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css'
+import Sidebar from './components/sidebar/Sidebar';
+import Topbar from './components/topbar/Topbar';
+// import Sidebar from "./components/sidebar/Sidebar";
+// import Topbar from "./components/topbar/Topbar";
+// import UserList from "./pages/userList/UserList";
+// import User from "./pages/user/User";
+// import NewUser from "./pages/newUser/NewUser";
+// import ProductList from "./pages/productList/ProductList";
+// import Product from "./pages/product/Product";
+// import NewProduct from "./pages/newProduct/NewProduct";
+import Home from "./pages/home/Home";
+import NewProduct from './pages/newProduct/NewProduct';
+import NewUser from './pages/newUser/NewUser';
+import Product from './pages/product/Product';
+import ProductList from './pages/productList/ProductList';
+import User from './pages/user/User';
+import UserList from './pages/userList/UserList';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Topbar />
+      <div className="container">
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<Home />} />            
+          <Route path="/users" element={<UserList />} />            
+          <Route path="/user/:userId" element={<User />} />            
+          <Route path="/newUser" element={<NewUser />} />            
+          <Route path="/products" element={<ProductList />} />            
+          <Route path="/product/:productId" element={<Product/>} />            
+          <Route path="/newproduct" element={<NewProduct/>} />            
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
